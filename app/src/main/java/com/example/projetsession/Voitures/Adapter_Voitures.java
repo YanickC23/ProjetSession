3726f78bfe462e24745e23d37b3fc7efd8a1ae77
Yanick Chrétien <yanick.chretien.01@edu.cegeptr.qc.ca>
2020-10-27 19:18:32 -0400

    Push fin journée 27 Octobre

--- app/src/main/java/com/example/projetsession/Voitures/Adapter_Voitures.java
+++ app/src/main/java/com/example/projetsession/Voitures/Adapter_Voitures.java
@@ -1,7 +1,63 @@
 package com.example.projetsession.Voitures;
 
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.projetsession.Objets.Voiture;
+import com.example.projetsession.R;
+
+import java.util.List;
+
+public class Adapter_Voitures extends RecyclerView.Adapter<Adapter_Voitures.MonViewHolder> {
 
 
-public class Adapter_Voitures {
+    private List<Voiture> listeVoitures;
+
+
+    public Adapter_Voitures(List<Voiture>list){
+        this.listeVoitures = list;
+    }
+
+
+    @NonNull
+    @Override
+    public MonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.fragment_liste__mes_voitures, parent, false);
+        return new MonViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MonViewHolder holder, int position){
+
+    }
+
+
+    @Override
+    public int getItemCount(){
+        return listeVoitures.size();
+    }
+
+
+
+
+
+
+    public class MonViewHolder extends RecyclerView.ViewHolder{
+
+
+
+        public MonViewHolder(View view){
+            super(view);
+
+        }
+    }
+
+
 }
