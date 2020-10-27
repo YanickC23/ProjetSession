044ac43b3fd672e566e56ce90ae3eb466f807182
Yanick Chrétien <yanick.chretien.01@edu.cegeptr.qc.ca>
2020-10-27 18:13:07 -0400

    push 27 Octobre 18h

--- app/src/main/java/com/example/projetsession/Voitures/AjoutVoiture.java
+++ app/src/main/java/com/example/projetsession/Voitures/AjoutVoiture.java
@@ -4,23 +4,36 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.projetsession.R;
 
 
 public class AjoutVoiture extends Fragment {
 
+    InterfaceAjoutVoiture  interfaceAjoutVoiture;
+
+    EditText edxMarque, edxModele, edxAnnee, edxCategorie,
+                edxDescription, edxTarif, edxValeur;
+
+    Button btnEnr;
 
     public AjoutVoiture() {
         // Required empty public constructor
     }
 
 
+    public interface InterfaceAjoutVoiture{
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,7 @@ public class AjoutVoiture extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        interfaceAjoutVoiture = (InterfaceAjoutVoiture)context;
 
     }
 
@@ -47,6 +61,26 @@ public class AjoutVoiture extends Fragment {
         super.onDetach();
 
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+            edxMarque = view.findViewById(R.id.edxMarque);
+            edxModele = view.findViewById(R.id.edxModele);
+            edxAnnee = view.findViewById(R.id.edxAnnee);
+            edxDescription = view.findViewById(R.id.edxDescription);
+            edxTarif = view.findViewById(R.id.edxTarif);
+            edxValeur = view.findViewById(R.id.edxValeur);
+            btnEnr = view.findViewById(R.id.btnEnrVoiture);
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+    }
+
 
 
 }
