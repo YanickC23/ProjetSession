4c1bad29e8443ce2bcb5d5c4fafe8aa01d250097
Yanick Chrétien <yanick.chretien.01@edu.cegeptr.qc.ca>
2020-10-28 22:26:09 -0400

    Push fin journée 28 Octobre

--- app/src/main/java/com/example/projetsession/Clients/GestionClients.java
+++ app/src/main/java/com/example/projetsession/Clients/GestionClients.java
@@ -1,8 +1,10 @@
-package com.example.projetsession;
+package com.example.projetsession.Clients;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.projetsession.R;
 
 public class GestionClients extends AppCompatActivity {
 
