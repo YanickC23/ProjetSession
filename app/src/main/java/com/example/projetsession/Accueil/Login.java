657ad48b47ef0c8b38a86cbfaea0c6ed155f4040
Yanick Chrétien <yanick.chretien.01@edu.cegeptr.qc.ca>
2020-11-18 14:50:10 -0500

    Push Mercredi 18 Nov 13:00h

---
+++ app/src/main/java/com/example/projetsession/Accueil/Login.java
@@ -0,0 +1,46 @@
+package com.example.projetsession.Accueil;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.projetsession.R;
+
+
+public class Login extends Fragment {
+
+
+    public Login() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+
+}
